b17a4f6fa40a7576f65eae4e04c29bc7acf3802f
lee <lee.nuckols@gmail.com>
2023-09-02 11:33:47 -0500

    added french greeting to hello world.

--- hello/hello.go
+++ hello/hello.go
@@ -4,13 +4,16 @@ import "fmt"
 
 const englishHelloPrefix = "Hello"
 const spanishHelloPrefix = "Hola"
+const frenchHelloPrefix = "Bonjour"
 
 func Hello(name string, language string) string  {
   if name == ""{
     name = "World"
   }
   if language == "Spanish" {
-    return fmt.Sprintf("%s, %s.", spanishHelloPrefix ,name)
+    return fmt.Sprintf("%s, %s.", spanishHelloPrefix, name)
+  } else if language == "French" {
+    return fmt.Sprintf("%s, %s.", frenchHelloPrefix, name)
   }
   return fmt.Sprintf("%s, %s.", englishHelloPrefix, name)
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -18,6 +18,11 @@ t.Run("In Spanish", func(t *testing.T) {
   want := "Hola, Elodie."
   assertCorrectMessage(t, got, want)
 })
+t.Run("In French", func(t *testing.T) {
+  got := Hello("Jean", "French")
+  want := "Bonjour, Jean."
+  assertCorrectMessage(t, got, want)
+})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
