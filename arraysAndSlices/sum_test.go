7f157848c7de24a19fc4a020667ee33b670d71a9
lee <lee.nuckols@gmail.com>
2023-09-02 15:53:24 -0500

    added function to sum multiple slices of numbers

--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+  "reflect"
+  "testing"
+)
 
 func TestSum(t *testing.T) {
   t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -18,6 +21,15 @@ func TestSum(t *testing.T) {
     want := 6
     assessCorrectSum(t, got, want, numbers)
   })
+}
+
+func TestSumAll(t *testing.T)  {
+  got := SumAll([]int{1,2}, []int{0,9})
+  want := []int{3,9}
+
+  if !reflect.DeepEqual(got, want) {
+    t.Errorf("got %v want %v", got, want)
+  }
 }
 
 func assessCorrectSum(t testing.TB, got, want int, numbers []int) {
