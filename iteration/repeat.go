555013e5a6de8d1d58bb72c0468af5c9f8a0a9db
lee <lee.nuckols@gmail.com>
2023-09-02 14:26:22 -0500

    Added ability to request number of repeats

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,8 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(character string) string {
+func Repeat(character string, number int) string {
   var repeated string
-  for i := 0; i < repeatCount; i++ {
+  for i := 0; i < number; i++ {
     repeated += character
   }
   return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,10 +3,26 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-  repeated := Repeat("a")
-  expected := "aaaaa"
+  t.Run("Test Repeating 5 times", func(t *testing.T) {
+    repeated := Repeat("a", 5)
+    expected := "aaaaa"
+    assertCorrectResponse(t, expected, repeated)
+  })
+  t.Run("Test Repeating 6 times", func (t *testing.T)  {
+  repeated := Repeat("b", 6)
+  expected := "bbbbbb"
+  assertCorrectResponse(t, expected, repeated)
+  })
+}
 
+func assertCorrectResponse (t testing.TB, expected, repeated string) {
   if repeated != expected {
-    t.Errorf("expected %q but got %q", expected, repeated)
+    t.Errorf("expected %q, but got %q", expected, repeated)
+  }
+}
+
+func BenchmarkRepeat(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    Repeat("a", 5)
   }
 }
