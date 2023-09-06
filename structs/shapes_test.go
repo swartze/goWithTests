8ec1e41fc6d45b1b4c2cd794e60e3b79571d1ab3
lee <lee.nuckols@gmail.com>
2023-09-06 11:19:26 -0500

    Added shape interface and refactored shapes_test.go to use table tests.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -15,11 +15,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	for _, tt := range areaTests {
+		// using tt.name from the casse to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("got %g want %g", got, tt.hasArea)
+			}
+		})
 	}
 }
