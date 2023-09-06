3171e41afb7abc9fd0800eca890525b32ff77648
lee <lee.nuckols@gmail.com>
2023-09-06 10:29:21 -0500

    added Rectangle struct to shapes.go

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -14,7 +15,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
 	want := 72.0
 
 	if got != want {
